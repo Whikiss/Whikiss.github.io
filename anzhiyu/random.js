93a01787c5bb0caedc2111f56967ced2421b7311
Whikiss <justrosekiss@gmail.com>
2025-09-22 21:34:33 +0800

    Site updated: 2025-09-22 21:34:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/24/java小游戏/","2025/05/21/我一年来所积累的与学习有关的东西/","2025/01/28/如何在本地部署Deepseek/","2025/03/30/未竟之事/"];function toRandomPost(){
+var posts=["2025/01/24/java小游戏/","2025/05/21/我一年来所积累的与学习有关的东西/","2025/01/28/如何在本地部署Deepseek/","2025/03/30/未竟之事/","2025/09/22/《MySQL必知必会》总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
